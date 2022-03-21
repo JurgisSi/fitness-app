e9410f9826ed3d965121fff462d8406eb85e1cfe
Jurgis Sidlauskas <sidlauskas.jurgis@gmail.com>
2022-03-22 01:25:24 +0200

    feat: implement logout

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -6,13 +6,15 @@ import {
 import { Store } from "store";
 import { Observable } from "rxjs/Observable";
 import { Subscription } from "rxjs/Subscription";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
     <div>
-      <h1>{{ user$ | async | json }}</h1>
+      <app-header [user]="user$ | async" (logout)="onLogout()"></app-header>
+      <app-nav *ngIf="(user$ | async)?.authenticated"></app-nav>
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
@@ -23,11 +25,20 @@ export class AppComponent implements OnInit, OnDestroy {
   user$: Observable<User>;
   subscription: Subscription;
 
-  constructor(private store: Store, private authService: AuthService) {}
+  constructor(
+    private store: Store,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.subscription = this.authService.auth$.subscribe();
     this.user$ = this.store.select<User>("user");
+  }
+
+  async onLogout() {
+    await this.authService.logoutUser();
+    this.router.navigate(["/auth/login"]);
   }
 
   ngOnDestroy(): void {
